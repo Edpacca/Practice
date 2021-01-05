fa6fc81ab130fad8c55170e913f3bb0548039a41
Edpacca <eddie@pace.me.uk>
2021-01-05 15:14:04 +0000

    Fibonacci calculator

---
+++ CSharp/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCode
+{
+    class Fibonacci
+    {
+        public int CalculateFibonnacciNumber(int n)
+        {
+            int fibonacci0 = 0;
+            int fibonacci1 = 1;
+            int fibonacciN = 0;
+
+            while(n > 0)
+            {
+                fibonacciN = fibonacci0 + fibonacci1;
+                fibonacci0 = fibonacci1;
+                fibonacci1 = fibonacciN;
+                n--;
+            }
+
+            return fibonacciN;
+        }
+    }
+}
