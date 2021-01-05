bbe017737a2e65d9c6dbade19c145710090b77fe
Edpacca <eddie@pace.me.uk>
2021-01-05 15:13:33 +0000

    initial program commit

---
+++ CSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PracticeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessChecker chessChecker = new ChessChecker();
+
+            var validPositions = chessChecker.ValidKnightPositions('b', 2);
+
+            foreach (var postition in validPositions)
+            {
+                Console.WriteLine(postition);
+            }
+        }
+
+        public static int UserInputToInteger(string message)
+        {
+            string userInput;
+            int output;
+
+            do
+            {
+                Console.Write(message);
+                userInput = Console.ReadLine();
+            } 
+            while (!int.TryParse(userInput, out output));
+
+            return output;
+        }
+    }
+}
