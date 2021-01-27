c2876ccf32e45aa1b8d204c82698a71f9d6d6e14
Edpacca <eddie@pace.me.uk>
2021-01-27 10:52:54 +0000

    FizzBuzz solution example

---
+++ CSharp/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PracticeCode
+{
+    class FizzBuzz
+    {
+        int fizzMultiple = 3;
+        int buzzMultiple = 5;
+        string fizzMessage = "Fizz";
+        string buzzMessage = "Buzz";
+        int countTo = 100;
+        bool isMultiple = false;
+
+
+        public void FizzBuzzer()
+        {
+            for (int i = 1; i <= countTo; i++)
+            {
+                if (i % fizzMultiple == 0)
+                {
+                    Console.Write(fizzMessage);
+                    isMultiple = true;
+                }
+
+                if (i % buzzMultiple == 0)
+                {
+                    Console.Write(buzzMessage);
+                    isMultiple = true;
+                }
+
+                if (!isMultiple)
+                    Console.Write(i);
+
+                isMultiple = false;
+                Console.WriteLine("");
+            }
+        }
+    }
+}
