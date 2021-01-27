ebb85773ed311c93507947dd36becb0f27bd556a
Edpacca <eddie@pace.me.uk>
2021-01-27 10:53:24 +0000

    practice code solutions

---
+++ CSharp/SimpleCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCode
+{
+    class SimpleCode
+    {
+        public static int UserInputToInteger(string message)
+        {
+            string userInput;
+            int output;
+
+            do
+            {
+                Console.Write(message);
+                userInput = Console.ReadLine();
+            }
+            while (!int.TryParse(userInput, out output));
+
+            return output;
+        }
+    }
+}
---
+++ CSharp/Sort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace PracticeCode
+{
+    class Sort
+    {
+        public static int[] SortArray1(int[] array)
+        {
+            int buffer;
+            int sortIndex = 1;
+            int indexBuffer = 1;
+            int length = array.Length;
+
+            while (sortIndex < length)
+            {
+                indexBuffer = sortIndex;
+
+                while (indexBuffer > 0)
+                {
+                    if (array[indexBuffer] < array[indexBuffer - 1])
+                    {
+                        buffer = array[indexBuffer - 1];
+                        array[indexBuffer - 1] = array[indexBuffer];
+                        array[indexBuffer] = buffer;
+                    }
+
+                    indexBuffer--;
+                }
+
+                sortIndex++;
+            }
+
+            PrintArray(array);
+            return array;
+        }
+
+        public static int[] SortArray(int[] array)
+        {
+            int buffer;
+            int length = array.Length;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        buffer = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = buffer;
+                    }
+                }
+            }
+
+            PrintArray(array);
+            return array;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (var value in array)
+                Console.WriteLine(value);
+        }
+
+
+
+    }
+}
